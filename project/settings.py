6973e06f244a43967360c916af6140154f7bd7bb
heba <hebaashraf81@gmail.com>
2023-01-14 23:33:57 +0200

    third  commit

--- project/settings.py
+++ project/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD': '551984',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
